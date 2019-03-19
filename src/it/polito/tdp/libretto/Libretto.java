f4f9e3aabbbbed8688898c4035d783bfefe16380
Giulia <Giulia@Giulia-PC>
2019-03-19 12:01:18 +0100

    completato punto 4

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -1,5 +1,72 @@
 package it.polito.tdp.libretto;
 
+import java.util.*;
+
 public class Libretto {
+	
+	private List<Voto> voti;
+
+	public Libretto() {
+		super();
+		this.voti = new ArrayList<Voto>();
+	}
+	
+	public void add(Voto v) {
+		voti.add(v);
+	}
+	
+//	public void StampaVoti(int voto) {}
+//	
+//	public String StampaVoti2(int voto) {}
+//	meglio il 3° metodo:
+	public List<Voto> cercaVoti(int punti) {
+		List<Voto> result = new ArrayList<Voto>();
+		
+		for(Voto v : this.voti) {
+			if(v.getVoto()==punti)
+				result.add(v);
+		}
+		return result;
+	}
+	
+	
+	/**
+	 * Ricerca un {@link Voto} relativo al corso di cui è specificato il nome
+	 * 
+	 * @param nomeEsame nome del corso da ricercare
+	 * @return 
+	 */
+	public Voto cercaEsame(String nomeEsame) {
+		Voto voto = new Voto(0, nomeEsame, null);
+		int pos = this.voti.indexOf(voto);
+		if(pos==-1)
+			return null;
+		else
+			return this.voti.get(pos); // non uso più for each
+	}
+	
+	/**
+	 * Dato un Voto, determina se esiste già un voto con uguale corso e punteggio.
+	 * 
+	 * @param nomeEsame del corso da ricercare
+	 * @return {@code true} se trova corso e punteggio uguali,
+	 * {@code false} se non trova il corso o lo trova con voto diverso
+ 	 */
+	public boolean esisteGiaVoto(Voto v) {
+		
+		int pos = this.voti.indexOf(v);
+		if(pos==-1)
+			return false;
+		else 
+			return v.getVoto()==this.voti.get(pos).getVoto();
+		
+//		Voto trovato = this.cercaEsame(v.getCorso());
+//		if(trovato==null)
+//			return false;
+//		if(trovato.getVoto()==v.getVoto())
+//			return true;
+//		else
+//			return false; 
+	}
 
 }
--- src/it/polito/tdp/libretto/Voto.java
+++ src/it/polito/tdp/libretto/Voto.java
@@ -1,5 +1,75 @@
 package it.polito.tdp.libretto;
 
+// import java.sql.Date;
+import java.time.LocalDate;
+
 public class Voto {
+	private int punti;
+	private String corso;
+	private LocalDate data; // /Calendar
+	
+	public Voto(int punti, String corso, LocalDate data) {
+		super();
+		this.punti = punti;
+		this.corso = corso;
+		this.data = data;
+	}
+
+	public int getVoto() {
+		return punti;
+	}
+
+	public void setVoto(int punti) {
+		this.punti = punti;
+	}
+
+	public String getCorso() {
+		return corso;
+	}
+
+	public void setCorso(String corso) {
+		this.corso = corso;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	@Override
+	public String toString() {
+		return "Voto [punti=" + punti + ", corso=" + corso + ", data=" + data + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((corso == null) ? 0 : corso.hashCode());
+		result = prime * result + ((data == null) ? 0 : data.hashCode());
+		result = prime * result + punti;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Voto other = (Voto) obj;
+		if (corso == null) {
+			if (other.corso != null)
+				return false;
+		} else if (!corso.equals(other.corso))
+			return false;
+		return true;
+	}
+	
 
 }
